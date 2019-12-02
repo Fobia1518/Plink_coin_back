e398b29c0ad74a55f69753c6dc883edb8e5f106d
Sergio Fabian Ortiz Valencia <seortiz@personalsoft.com.co>
2019-12-02 16:08:03 -0500

    Cambio nombre variable puerto

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 const { User, Coin } = require('./models');
 const jwt = require('jsonwebtoken');
 const config = require('./config/jwt');
